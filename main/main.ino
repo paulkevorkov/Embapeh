7bf4549fb511d56a60d249da089ee048d540f963
Paul Kevorkov <paul.kevorkov@mail.mcgill.ca>
2025-02-08 15:27:53 -0500

    controller reading

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,47 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ezButton.h>
 
+#define VRX_PIN  A1 // Arduino pin connected to VRX pin
+#define VRY_PIN  A0 // Arduino pin connected to VRY pin
+#define SW_PIN   2  // Arduino pin connected to SW  pin
+
+ezButton button(SW_PIN);
+
+int xValue = 0; // To store value of the X axis
+int yValue = 0; // To store value of the Y axis
+int bValue = 0; // To store value of the button
+
+void setup() {
+  Serial.begin(9600) ;
+  button.setDebounceTime(50); // set debounce time to 50 milliseconds
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  button.loop(); // MUST call the loop() function first
 
+  // read analog X and Y analog values
+  xValue = analogRead(VRX_PIN);
+  yValue = analogRead(VRY_PIN);
+
+  // Read the button value
+  bValue = button.getState();
+
+  if (button.isPressed()) {
+    Serial.println("The button is pressed");
+    // TODO do something here
+  }
+
+  if (button.isReleased()) {
+    Serial.println("The button is released");
+    // TODO do something here
+  }
+
+  // print data to Serial Monitor on Arduino IDE
+  Serial.print("x = ");
+  Serial.print(xValue);
+  Serial.print(", y = ");
+  Serial.print(yValue);
+  Serial.print(" : button = ");
+  Serial.println(bValue);
+
+  delay(50);
 }
