bbff468aa2a9e10028e04134b3ba2ec6d866d288
souhailelhayani <souhail.200308@gmail.com>
2025-02-08 17:04:15 -0500

    forward and backward but no stop

--- main/main.ino
+++ main/main.ino
@@ -5,13 +5,13 @@
 #define SW_PIN   2  // Arduino pin connected to SW  pin
 #define AL1 5  // Motor A pins
 #define AL2 6
-#define AR1 10 // Motor B pins
-#define AR2 11
+#define AR1 11 // Motor B pins
+#define AR2 10
 
 #define LEFT_THRESHOLD  400
 #define RIGHT_THRESHOLD 800
-#define UP_THRESHOLD    400
-#define DOWN_THRESHOLD  800
+#define UP_THRESHOLD    550
+#define DOWN_THRESHOLD  450
 
 #define COMMAND_NO     0x00
 #define COMMAND_LEFT   0x01
@@ -53,18 +53,18 @@ void setup() {
 
 
 int  input = 0;
-void forward() {          //function of forward 
-  analogWrite(AL1, 120);
+void forward(int x, int y) {          //function of forward 
+  analogWrite(AL1, y);
   analogWrite(AL2, 0);
-  analogWrite(AR1, 120);
+  analogWrite(AR1, y);
   analogWrite(AR2, 0);
 }
 
-void backward() {         //function of backward
+void backward(int x, int y) {         //function of backward
   analogWrite(AL1, 0);
-  analogWrite(AL2, 120);
+  analogWrite(AL2, abs(y));
   analogWrite(AR1, 0);
-  analogWrite(AR2, 120);
+  analogWrite(AR2, abs(y));
 }
 
 void stop() {              //function of stop
@@ -112,9 +112,9 @@ void loop() {
     command = command | COMMAND_RIGHT;
 
   // check up/down commands
-  if (yValue < UP_THRESHOLD)
+  if (yValue > UP_THRESHOLD)
     command = command | COMMAND_UP;
-  else if (yValue > DOWN_THRESHOLD)
+  else if (yValue < DOWN_THRESHOLD)
     command = command | COMMAND_DOWN;
 
   // NOTE: AT A TIME, THERE MAY BE NO COMMAND, ONE COMMAND OR TWO COMMANDS
@@ -143,25 +143,38 @@ void loop() {
   delay(50);
 
   // send data only when you receive data:
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    incomingByte = Serial.read();
-    input = incomingByte - 48; //convert ASCII code of numbers to 1,2,3
+  // if (Serial.available() > 0) {
+  //   // read the incoming byte:
+  //   incomingByte = Serial.read();
+  //   input = incomingByte - 48; //convert ASCII code of numbers to 1,2,3
 
-  switch (input) { 
-    case 1:         // if input=1 ....... motors turn forward
-      forward();
-      break;
-    case 2:         // if input=2 ....... motors turn backward
-      backward();
-      break;
-    case 3:         // if input=1 ....... motors turn stop
-      stop();
-      break;
+  // switch (input) { 
+  //   case 1:         // if input=1 ....... motors turn forward
+  //     forward();
+  //     break;
+  //   case 2:         // if input=2 ....... motors turn backward
+  //     backward();
+  //     break;
+  //   case 3:         // if input=1 ....... motors turn stop
+  //     stop();
+  //     break;
+  // }
+
+
+  if(command & COMMAND_UP) {
+    forward(xValue, yValue - 500);
+  } else if(command & COMMAND_DOWN) {
+    backward(xValue, 500 - yValue);
+  } else {
+    stop();
+    Serial.println("stopping");
   }
+
+
+
   delay(200);
   input=0;
-  }
+  
 }
 
 
