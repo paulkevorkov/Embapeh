39bd140734b103eb82d6f354834f45aa4fcc3b93
Paul Kevorkov <paul.kevorkov@mail.mcgill.ca>
2025-02-08 16:32:56 -0500

    should tell direction now

--- main/main.ino
+++ main/main.ino
@@ -8,6 +8,17 @@
 #define AR1 10 // Motor B pins
 #define AR2 11
 
+#define LEFT_THRESHOLD  400
+#define RIGHT_THRESHOLD 800
+#define UP_THRESHOLD    400
+#define DOWN_THRESHOLD  800
+
+#define COMMAND_NO     0x00
+#define COMMAND_LEFT   0x01
+#define COMMAND_RIGHT  0x02
+#define COMMAND_UP     0x04
+#define COMMAND_DOWN   0x08
+
 ezButton button(SW_PIN);
 
 // controller
@@ -63,6 +74,8 @@ void stop() {              //function of stop
   digitalWrite(AR2, LOW);
 }
 
+int command = COMMAND_NO;
+
 void loop() {
   button.loop(); // MUST call the loop() function first
 
@@ -90,6 +103,42 @@ void loop() {
   Serial.print(yValue);
   Serial.print(" : button = ");
   Serial.println(bValue);
+
+  command = COMMAND_NO;
+  // check left/right commands
+  if (xValue < LEFT_THRESHOLD)
+    command = command | COMMAND_LEFT;
+  else if (xValue > RIGHT_THRESHOLD)
+    command = command | COMMAND_RIGHT;
+
+  // check up/down commands
+  if (yValue < UP_THRESHOLD)
+    command = command | COMMAND_UP;
+  else if (yValue > DOWN_THRESHOLD)
+    command = command | COMMAND_DOWN;
+
+  // NOTE: AT A TIME, THERE MAY BE NO COMMAND, ONE COMMAND OR TWO COMMANDS
+
+  // print command to serial and process command
+  if (command & COMMAND_LEFT) {
+    Serial.println("COMMAND LEFT");
+    // TODO: add your task here
+  }
+
+  if (command & COMMAND_RIGHT) {
+    Serial.println("COMMAND RIGHT");
+    // TODO: add your task here
+  }
+
+  if (command & COMMAND_UP) {
+    Serial.println("COMMAND UP");
+    // TODO: add your task here
+  }
+
+  if (command & COMMAND_DOWN) {
+    Serial.println("COMMAND DOWN");
+    // TODO: add your task here
+  }
 
   delay(50);
 
